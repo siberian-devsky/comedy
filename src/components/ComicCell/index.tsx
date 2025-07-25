507bb55805fc3241ee0e559ce8c2fec3c23f42bd
Jeff Martin <chess.logic@gmail.com>
2025-07-24 18:30:46 -0700

    whatever

--- src/components/ComicCell/index.tsx
+++ src/components/ComicCell/index.tsx
@@ -27,12 +27,11 @@ export default function ComicCell({
 			id={id.toString()}
 			className={clsx(
 				'w-full h-48 mx-2 rounded-xl flex flex-col items-center justify-center',
-				'border-2 border-icdb transition-all duration-100',
+				'border-2 transition-all duration-100',
 				(comicOnStage?.id === id) 
 					&& 'shadow-lg shadow-orange-500 border border-orange-500/50'
 			)}
 		>
-			{hometown}
 			<button
 				onClick={handleClick}
 				className={clsx(
@@ -40,10 +39,11 @@ export default function ComicCell({
 					'transition-transform hover:scale-[1.02] cursor-pointer',
 					// 'text-sm md:text-lg'
 				)}
-			>
+				>
 				<span className='text-slate-800 bg-icdb pl-3 pr-1 rounded-l-md'>{nameParts[0]}</span>
 				<span className='text-icdb bg-slate-800 pr-3 pl-1 rounded-r-md'>{nameParts[1]}</span>
 			</button>
+			{hometown}
 		</div>
 	)
 }
