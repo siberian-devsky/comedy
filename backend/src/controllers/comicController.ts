74ab99f460deb3d74e5bccc888a0b0cefdc49d8e
Jeff Martin <chess.logic@gmail.com>
2025-07-24 19:51:17 -0700

    initial branch commit

--- backend/src/controllers/comicController.ts
+++ backend/src/controllers/comicController.ts
@@ -67,3 +67,37 @@ export async function GetOneComicByName(
 		})
 	}
 }
+
+// GET test to https://www.themoviedb.org/
+export async function TmdbTest(
+	req: Request,
+	res: Response
+): Promise<Response> {
+
+	const url = 'https://api.themoviedb.org/3/authentication'
+	const options = {
+		method: 'GET',
+		headers: {
+			accept: 'application/json',
+			Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOGY2YzQ0Y2VjNTdlNDlmNDgyMzJmYzU2NzAwM2IxYSIsIm5iZiI6MTc1MjYxODk4MC44NzgsInN1YiI6IjY4NzZkN2U0ZjE4NzFhZGY4ODA1MjIyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KwDn0qgfoorhK4KXcywssqhSl5jSljy8qiXejVbu168'
+		} 
+	}
+
+	try {
+		const res = await fetch(url, options)
+		const data = await res.json()
+
+		if (!data) {
+			return res.status(404).json({
+				status: 404,
+				message: 'foo',
+			})
+		}
+	} catch (err) {
+		console.error(err)
+		return res.status(500).json({
+			status: 500,
+			mesage: 'fuckered up err',
+		})
+	}
+}
--- backend/src/routes/comicApiRoutes.ts
+++ backend/src/routes/comicApiRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import { GetAllComics, GetOneComicByName } from '../controllers/comicController'
+import { GetAllComics, GetOneComicByName, TmdbTest } from '../controllers/comicController'
 
 const router = Router()
 
 router.get('/comics/all', GetAllComics)
 router.post('/comics', GetOneComicByName)
+
+//>add here
+router.get('/TmdbTest', TmdbTest)
 
 export default router
